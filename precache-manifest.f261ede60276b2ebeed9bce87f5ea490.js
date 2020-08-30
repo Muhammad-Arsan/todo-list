ebc8e71b184ba3176b7bb994d02960b81b88d806
Muhammad-Arsan <arsan924@gmail.com>
2020-08-30 18:05:07 +0500

    Updates

---
+++ precache-manifest.f261ede60276b2ebeed9bce87f5ea490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a275c3bcf9ac47d3ac296f6c51918252",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f087f6523a79e9bd7213",
+    "url": "/todo-list/static/css/main.51c1e6b2.chunk.css"
+  },
+  {
+    "revision": "13fd2c82012ebb68dece",
+    "url": "/todo-list/static/js/2.2d7d9530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.2d7d9530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f087f6523a79e9bd7213",
+    "url": "/todo-list/static/js/main.a5460c0f.chunk.js"
+  },
+  {
+    "revision": "66aaff6f27e1f49f61f5",
+    "url": "/todo-list/static/js/runtime-main.205935bf.js"
+  }
+]);
